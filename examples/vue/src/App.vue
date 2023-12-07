f67b34ecb40f87e23b25be6e122265f524953829
razzh <razzhavenir@163.com>
2023-12-07 22:11:00 +0800

    perf: update example

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { useProvideIconContext } from '@twist-space/vue3-icons'
-import { AiTagTwotone, AiAccountBookFilled, AiLeftCircleFilled, AiAlignCenterOutlined } from '@twist-space/vue3-icons/ai'
-useProvideIconContext({
-  color: 'red'
-})
+import { IconConfigProvider } from '@twist-space/vue3-icons'
+import {
+  AiTagTwotone,
+  AiAccountBookFilled,
+  AiLeftCircleFilled
+} from '@twist-space/vue3-icons/ai'
 const onClick = () => {
   console.log('click')
 }
@@ -11,9 +12,12 @@ const onClick = () => {
 
 <template>
   <div>
-    <AiTagTwotone :size="100" @click="onClick" title="razzh"></AiTagTwotone>
+    <IconConfigProvider color="blue" :size="100">
+      <AiTagTwotone />
+    </IconConfigProvider>
+    <AiTagTwotone />
+    <AiTagTwotone :size="100" @click="onClick" title="razzh" />
     <AiAccountBookFilled :size="30" spin class="twist-aa" :style="{color: 'blue'}" />
-    <AiLeftCircleFilled :size="100" dsad="99"></AiLeftCircleFilled>
-    <AiAlignCenterOutlined></AiAlignCenterOutlined>
+    <AiLeftCircleFilled :size="100" dsad="99" />
   </div>
 </template>
