2e41fda657393d8ebc7366d2fbd3e39434e67a9d
razzh <razzhavenir@163.com>
2023-12-28 22:41:42 +0800

    chore: release v0.2.4

--- packages/twist-icons/scripts/version.ts
+++ packages/twist-icons/scripts/version.ts
@@ -1,3 +1,3 @@
-export const versionVue2 = '0.2.1'
-export const versionVue3 = '0.2.1'
-export const versionReact = '0.2.3'
+export const versionVue2 = '0.2.4'
+export const versionVue3 = '0.2.4'
+export const versionReact = '0.2.4'
