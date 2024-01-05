e49b31df0a0a5ef071d16daa83957e18958b728c
razzh <razzhavenir@163.com>
2024-01-05 23:04:25 +0800

    chore(demo): remove vue2 demo

--- examples/vite-vue2/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="root">
-    <IconProvider :size="60">
-      <AiThunderboltFilled color="#906efe" />
-      <TiModeDark :size="30" />
-    </IconProvider>
-  </div>
-</template>
--- examples/vite-vue2/src/main.ts
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './index.css'
-import './App.css'
-
-new Vue(App).$mount('#app')
