28f255f20845e1262d167a522f056b747638833b
razzh <razzhavenir@163.com>
2024-01-19 22:31:18 +0800

    chore: release v0.2.7

--- packages/twist-icons/scripts/version.ts
+++ packages/twist-icons/scripts/version.ts
@@ -1,3 +1,3 @@
-export const versionVue2 = '0.2.6'
-export const versionVue3 = '0.2.6'
-export const versionReact = '0.2.6'
+export const versionVue2 = '0.2.7'
+export const versionVue3 = '0.2.7'
+export const versionReact = '0.2.7'
