0299c00a4694019f6203d076e5cd72bc4f108047
razzh <razzhavenir@163.com>
2024-01-26 21:26:52 +0800

    build: release v0.2.10

--- packages/twist-icons/scripts/version.ts
+++ packages/twist-icons/scripts/version.ts
@@ -1,4 +1,4 @@
-export const versionVue2 = '0.2.9'
-export const versionVue3 = '0.2.9'
-export const versionReact = '0.2.9'
-export const version = '0.2.9'
+export const versionVue2 = '0.2.10'
+export const versionVue3 = '0.2.10'
+export const versionReact = '0.2.10'
+export const version = '0.2.10'
