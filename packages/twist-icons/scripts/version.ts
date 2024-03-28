25c6fc9a9a18ee22ab75e3dae48990f7020a02ab
razzh <razzhavenir@163.com>
2024-03-28 22:05:42 +0800

    chore: update version

--- packages/twist-icons/scripts/version.ts
+++ packages/twist-icons/scripts/version.ts
@@ -1,4 +1,4 @@
-export const versionVue2 = '0.2.15'
-export const versionVue3 = '0.2.15'
-export const versionReact = '0.2.15'
-export const version = '0.2.15'
+export const versionVue2 = '0.2.16'
+export const versionVue3 = '0.2.16'
+export const versionReact = '0.2.16'
+export const version = '0.2.16'
