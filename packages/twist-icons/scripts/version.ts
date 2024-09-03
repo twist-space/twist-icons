c073d99d7531fdb76933615fe6f1d3c5a07c1c94
razzh <razzhavenir@163.com>
2024-09-03 10:26:03 +0800

    fix: 部分图标丢失

--- packages/twist-icons/scripts/version.ts
+++ packages/twist-icons/scripts/version.ts
@@ -1,4 +1,4 @@
-export const versionVue2 = '0.3.1'
-export const versionVue3 = '0.3.1'
-export const versionReact = '0.3.1'
-export const version = '0.3.1'
+export const versionVue2 = '0.3.2'
+export const versionVue3 = '0.3.2'
+export const versionReact = '0.3.2'
+export const version = '0.3.2'
