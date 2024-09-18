fa5814f3f7166f5546ea9eac3d2f7d4c4ee0a253
razzh <razzhavenir@163.com>
2024-09-18 22:10:43 +0800

    fix: missing xmlns

--- packages/twist-icons/src/vue3/IconBase.ts
+++ packages/twist-icons/src/vue3/IconBase.ts
@@ -74,6 +74,7 @@ export const IconBase = defineComponent({
     return () => h(
       'svg',
       {
+        xmlns: 'http://www.w3.org/2000/svg',
         ...attrs,
         class: mergedClass.value,
         style: mergedStyles.value,
