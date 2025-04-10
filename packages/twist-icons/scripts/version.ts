216507d868c347251fae52ac07d537478198c9e1
razzh <razzhavenir@163.com>
2025-04-10 17:59:38 +0800

    chore: build icons

--- packages/twist-icons/scripts/version.ts
+++ packages/twist-icons/scripts/version.ts
@@ -1,4 +1,4 @@
-export const versionVue2 = '1.0.4'
-export const versionVue3 = '1.0.4'
-export const versionReact = '1.0.4'
-export const version = '1.0.4'
+export const versionVue2 = '1.0.6'
+export const versionVue3 = '1.0.5'
+export const versionReact = '1.0.5'
+export const version = '1.0.5'
